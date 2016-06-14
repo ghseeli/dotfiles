e5c13b211aa79353edaf02bf3dcc92c4d9a96985
George H. Seelinger <gseelinger@conversantmedia.com>
2016-06-14 11:31:32 -0500

    Added more to install.sh to setup vim.

--- install.sh
+++ install.sh
@@ -1,2 +1,17 @@
+BASE=$(pwd)
+
+# git identity
 git config --global user.name "George H. Seelinger"
+
+# vim setup
+if [ -f $HOME/.vimrc ];
+then
+    mv $HOME/.vimrc .vimrc.old
+fi
+if [ -f $HOME/.vim ];
+then
+    mv $HOME/.vim .vim.old
+fi
+ln -s $BASE/.vim/.vimrc $HOME/.vimrc
+ln -s $BASE/.vim $HOME/.vim
 ./install-pathogen.sh
