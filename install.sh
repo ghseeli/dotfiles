7e0f2532dc306e750c9ab8df4ecf56945d60a1aa
George H. Seelinger <ghseeli@gmail.com>
2016-06-22 16:59:52 -0500

    Trying harder to make scripts idempotent by checking for file vs symlink.

--- install.sh
+++ install.sh
@@ -1,27 +1,39 @@
 BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+function install_file_mv {
+    # Note that -h flag is used because the assumption
+    # is that if you have a symlink in place for these
+    # files, you already know what you are doing.
+    # 
+    # -h flag also helps ensure idempotency.
+
+    if [ -f $2 ] && ! [ -h $2 ];
+    then
+        if [ -f "${2}.local" ];
+        then
+            echo "Moving ${2}.local to ${2}.backup !"
+            mv ${2}.local ${2}.backup
+        fi
+        echo "Moving ${2} to ${2}.local"
+        mv ${2} ${2}.local
+    fi
+    if [ -h $2 ];
+    then
+        echo "$2 exists but not as a symbolic link. Leaving it as is."
+    else
+        ln -s $1 $2
+    fi
+}
+
 # git identity
-git config --global user.name "George H. Seelinger"
+install_file_mv $BASE/.gitconfig ~/.gitconfig
 
 # bash settings
-if [ -f ~/.bashrc ];
-then
-    mv ~/.bashrc ~/.bashrc.local
-fi
-
-ln -s $BASE/.bashrc ~/.bashrc
+install_file_mv $BASE/.bashrc ~/.bashrc
 
 # vim setup
-if [ -f $HOME/.vimrc ];
-then
-    mv $HOME/.vimrc $HOME/.vimrc.old
-fi
-if [ -f $HOME/.vim ];
-then
-    mv $HOME/.vim $HOME/.vim.old
-fi
-ln -s $BASE/.vim/.vimrc $HOME/.vimrc
-ln -s $BASE/.vim $HOME
+install_file_mv $BASE/.vim/.vimrc $HOME/.vimrc
+install_file_mv $BASE/.vim $HOME
 
 . $BASE/install/install-pathogen.sh
 . $BASE/install/install-vim-plugins.sh
--- install/install-pathogen.sh
+++ install/install-pathogen.sh
@@ -1,4 +1,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mkdir -p $DIR/../.vim/autoload $DIR/../.vim/bundle && \
-curl -LSso $DIR/../.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p $DIR/../.vim/autoload $DIR/../.vim/bundle 
+if ! [ -f $DIR/../.vim/autoload/pathogen.vim ]; then
+    curl -LSso $DIR/../.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
+
